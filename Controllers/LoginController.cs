02651f04ec861f76ef43284ef6ec71116c798004
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-21 19:28:43 +0330

    Create login and singup Controller and views  login and singup

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Firstproject.Asp.Net.Core.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SingupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Firstproject.Asp.Net.Core.Controllers
+{
+    public class SingupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
