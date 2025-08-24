7d03aa0f6320f324ab8a18b2a7d64ffe055366d4
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-24 17:41:01 +0330

    Product model creation

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Firstproject.Asp.Net.Core.Models
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public decimal ProductPrice { get; set; }
+        
+
+
+    }
+}
