fa760e897db2146b12ec7ed5a3f649876203d003
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-24 17:33:18 +0330

    Modifying the overall structure and replacing users with registration and login

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Firstproject.Asp.Net.Core.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Singup()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        
+    }
+}
