0d3fd27033b9e36ae88ab73538c3b558d645dddd
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-24 17:43:58 +0330

    Add product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Firstproject.Asp.Net.Core.Models; // Assuming Product model is defined in this namespace
+
+namespace Firstproject.Asp.Net.Core.Controllers
+{
+    public class ProductController : Controller
+    {
+
+
+        public IActionResult ShowProduct()
+        {
+
+
+            return View();
+        }
+    }
+}
