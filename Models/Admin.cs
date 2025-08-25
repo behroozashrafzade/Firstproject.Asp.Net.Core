6a26922437d7421bbb9741669afc92ba224a2e01
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-25 21:46:26 +0330

    addd models Admin

---
+++ Models/Admin.cs
@@ -0,0 +1,24 @@
+﻿namespace Firstproject.Asp.Net.Core.Models
+{
+    public class Admin
+    {
+        
+
+            public Guid AdminId { get; set; }
+
+            public string AdminUserName { get; set; }
+
+            public string AdminPassword { get; set; }
+
+            public string AdminEmail { get; set; }
+
+            public string AdminPhone { get; set; }
+
+
+
+
+
+        
+
+    }
+}
