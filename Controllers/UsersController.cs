21802b2b79c190395f61ea0c33997099466e3664
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-25 22:36:13 +0330

    add userslist in controller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,9 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Firstproject.Asp.Net.Core.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Firstproject.Asp.Net.Core.Controllers
 {
     public class UsersController : Controller
     {
+        User myUser = new User();
+
+        List<User> myUsers = new List<User>();
+
+        public UsersController()
+        {
+            myUsers.Add(
+             new User
+             {
+                 UserId = Guid.NewGuid(),
+                 UserName = "user1",
+                 UserPassword = "user1",
+                 UserEmail = "user1",
+                 UserPhone = "user1"
+             });
+
+            myUsers.Add(
+             new User
+             {
+                 UserId = Guid.NewGuid(),
+                 UserName = "user2",
+                 UserPassword = "user2",
+                 UserEmail = "user2",
+                 UserPhone = "user2"
+             });
+
+
+            myUsers.Add(
+            new User
+            {
+                UserId = Guid.NewGuid(),
+                UserName = "user3",
+                UserPassword = "user3",
+                UserEmail = "user3",
+                UserPhone = "user3"
+            });
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,7 +50,7 @@ namespace Firstproject.Asp.Net.Core.Controllers
 
         public IActionResult UsersList()
         {
-            return View();
+            return View(myUsers);
 
         }
 
